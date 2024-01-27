6f37cc57b5685ec4b78719d0965de0d520a1d1f0
John Venditti <jdv005@bucknell.edu>
2024-01-27 16:47:57 -0500

    Converting to a react app

---
+++ src/react-components/basic-container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Typography } from '@material-ui/core';
+
+const BasicContainer = () => {
+    return (
+        <Container>
+            <Typography variant="h1">Hello, World!</Typography>
+        </Container>
+    );
+};
+
+export default BasicContainer;
